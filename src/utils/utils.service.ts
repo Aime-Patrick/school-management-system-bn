c31422a290441c974ce145768945dfc673473496
Aime Patrick <aimepatrick64@gmail.com>
2025-03-31 01:25:49 +0200

    fix: replace bcrypt with bcryptjs for improved compatibility

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Injectable } from '@nestjs/common';
 import * as crypto from 'crypto';
 import { v2 as cloudinary } from 'cloudinary';
