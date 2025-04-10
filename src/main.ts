cef5f2c954134a4607d62996d7b0dcbe78ecf78e
Aime Patrick <aimepatrick64@gmail.com>
2025-04-10 21:41:46 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: "https://school-management-system-fn.vercel.app",
+    Credential:true
+  });
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true}));
